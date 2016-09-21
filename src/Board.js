ade2c0e2641f2c0c64e578a4f22bd1638538f4cd
David Walsh <dawalsh@gmail.com>
2016-09-21 11:48:58 -0700

    Implement minor diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -149,12 +149,26 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rows = this.rows();
+      return rows.reduce(function(memo, row, rowInd) {
+        var ind = (-1 * rowInd) + minorDiagonalColumnIndexAtFirstRow;
+        if (row[ind] === undefined) {
+          return memo;
+        } else {
+          return memo + row[ind];
+        }
+      }, 0) > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i = 0; i < (2*n-1); i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
