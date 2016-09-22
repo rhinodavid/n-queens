d3738c6417c2c8b44feb7235bf4b33e5fe5cc415
rhinodavid <dawalsh@gmail.com>
2016-09-21 21:01:42 -0700

    Fix bugs

--- src/liteBoard.js
+++ src/liteBoard.js
@@ -2,37 +2,39 @@ var LiteBoard = function LiteBoard(matrix, pieceType, pieces, prevVI, availableV
   this.matrix = matrix;
   this.n = matrix[0].length;
   this.pieces = pieces || 0;
-  this.prevVI = prevVI || -1;
-  this.availableVI = availableVI || _.range(n * n);
+  this.prevVI = prevVI === undefined ? -1 : prevVI;
+  this.availableVI = availableVI || _.range(this.n * this.n);
   this.pieceType = pieceType;
 };
 
 LiteBoard.prototype.viToRowColumn = function(vi) {
-  return [Math.floor(vi / this.n), vi % n];
+  return [Math.floor(vi / this.n), vi % this.n];
 };
 
 LiteBoard.prototype.rowColumnToVI = function(row, col) {
-  vi = row * n + col;
+  vi = row * this.n + col;
 };
 
 LiteBoard.prototype.addPieceAt = function(vi) {
-  if (availableVI.indexOf(vi) !== -1) {
+  if (this.availableVI.indexOf(vi) === -1) {
     return null;
   }
   var rowCol = this.viToRowColumn(vi);
-  var result = this.matrix.slice();
+  var result = this.matrix.map(function(x) {
+    return x.slice();
+  });
   result[rowCol[0]][rowCol[1]] = 1;
-  var newAvailableVI = availableVI.filter(function(oldVI) {
+  var newAvailableVI = this.availableVI.filter(function(oldVI) {
     var oldRowCol = this.viToRowColumn(oldVI);
-    if (oldVI[0] === rowCol[0] || oldVI[1] === rowCol[1]) {
+    if (oldRowCol[0] === rowCol[0] || oldRowCol[1] === rowCol[1]) {
       return false;
     }
     //if is queen
-    if (this.pieceType === 'queen' && (Math.abs(oldVI[0] - rowCol[0]) === Math.abs(oldVI[1] - rowCol[1]))) {
+    if (this.pieceType === 'queen' && (Math.abs(oldRowCol[0] - rowCol[0]) === Math.abs(oldRowCol[1] - rowCol[1]))) {
       return false;
     }
     return true;
-  });
+  }.bind(this));
 
   return new LiteBoard(result, this.pieceType, this.pieces + 1, vi, newAvailableVI);
 };
@@ -40,9 +42,9 @@ LiteBoard.prototype.addPieceAt = function(vi) {
 LiteBoard.prototype.generateValidChildBoards = function() {
   var validBoards = [];
 
-  for (var i = 0; i < availableVI.length; i++) {
-    if (availableVI[i] > prevVI) {
-      validBoards.push(this.addPieceAt(availableVI[i]));
+  for (var i = 0; i < this.availableVI.length; i++) {
+    if (this.availableVI[i] > this.prevVI) {
+      validBoards.push(this.addPieceAt(this.availableVI[i]));
     }
   }
 
